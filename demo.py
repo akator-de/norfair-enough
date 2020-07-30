tryolabs/norfair

First version working end to end on OpenPose demo

demo.py
@@ -16,7 +16,7 @@ class OpenposeDetector():
     def __call__(self, image):
         return self.detector.forward(image, False)
 
-def convert_and_filter_poses(poses):
+def convert_and_filter(poses):
     # Get final poses by filtering out parts of the detections we don't want to track
     body_parts_to_track_indices = [1, 8]  # Neck and midhip (torax length)
     poses = poses[:, body_parts_to_track_indices]
@@ -52,11 +52,9 @@ tracker = norfair.Tracker(distance_function=keypoints_distance)
 
 for frame in video:
     detected_poses = pose_detector(frame)
-    converted_detections = convert_and_filter_poses(detected_poses)
-    predictions = tracker.update(converted_detections, dt=3)
-    # norfair.draw_midpoint(frame, predictions)
-    # norfair.draw_pose(frame, converted_detections, colors.blue)
-    print(predictions)
-    video.write(frame)
-    # video.show(frame, downsample_ratio=4)
-
+    converted_detections = convert_and_filter(detected_poses)
+    predictions = tracker.update(converted_detections, dt=1)
+    norfair.draw_points(frame, converted_detections)
+    norfair.draw_predictions(frame, predictions)
+    # video.write(frame)
+    video.show(frame, downsample_ratio=4)

norfair/drawing.py
@@ -1,0 +1,61 @@
+import cv2
+import numpy as np
+
+def draw_points(frame, detections, radius=None, thickness=None, color=None):
+    if radius is None:
+        frame_scale = frame.shape[0] * frame.shape[1] / 400000
+        radius = int(max(frame_scale * 1.3, 1))
+    if thickness is None:
+        frame_scale = frame.shape[0] * frame.shape[1] / 4000000
+        thickness = int(max(frame_scale * 1.4, 1))
+    if color is None:
+        color = Color.red
+
+    for d in detections:
+        for point in d:
+            cv2.circle(frame, tuple(point.astype(int)), radius=radius, color=color, thickness=thickness)
+
+def draw_predictions(frame, objects, radius=None, thickness=None, color=None, id_size=None):
+    frame_scale = frame.shape[0] * frame.shape[1] / 400000
+    if radius is None:
+        radius = int(frame_scale)
+    if id_size is None:
+        id_size = int(frame_scale / 5)
+    if thickness is None:
+        thickness = int(frame_scale / 5)
+    if color is None:
+        color = Color.blue
+
+    for obj in objects:
+        for point in obj.prediction:
+            cv2.circle(frame, tuple(point.astype(int)), radius=radius, color=color, thickness=-1)
+
+        id_draw_position = centroid(obj.prediction)
+        cv2.putText(
+            frame, str(obj.id), id_draw_position, cv2.FONT_HERSHEY_SIMPLEX, id_size,
+            Color.yellow, thickness, cv2.LINE_AA
+        )
+
+def centroid(points):
+    length = points.shape[0]
+    sum_x = np.sum(points[:, 0])
+    sum_y = np.sum(points[:, 1])
+    return int(sum_x/length), int(sum_y/length)
+
+class Color():
+    green = (0, 128, 0)
+    white = (255, 255, 255)
+    olive = (0, 128, 128)
+    black = (0, 0, 0)
+    navy = (128, 0, 0)
+    red = (0, 0, 255)
+    maroon = (0, 0, 128)
+    grey = (128, 128, 128)
+    purple = (128, 0, 128)
+    yellow = (0, 255, 255)
+    lime = (0, 255, 0)
+    fuchsia = (255, 0, 255)
+    aqua = (255, 255, 0)
+    blue = (255, 0, 0)
+    teal = (128, 128, 0)
+    silver = (192, 192, 192)

akator-de/norfair-enough

demo.py
@@ -16,7 +16,7 @@ class OpenposeDetector():
     def __call__(self, image):
         return self.detector.forward(image, False)
 
-def convert_and_filter_poses(poses):
+def convert_and_filter(poses):
     # Get final poses by filtering out parts of the detections we don't want to track
     body_parts_to_track_indices = [1, 8]  # Neck and midhip (torax length)
     poses = poses[:, body_parts_to_track_indices]
@@ -52,11 +52,9 @@ tracker = norfair.Tracker(distance_function=keypoints_distance)
 
 for frame in video:
     detected_poses = pose_detector(frame)
-    converted_detections = convert_and_filter_poses(detected_poses)
-    estimates = tracker.update(converted_detections, dt=3)
-    # norfair.draw_midpoint(frame, estimates)
-    # norfair.draw_pose(frame, converted_detections, colors.blue)
-    video.write(frame)
-    print(estimates)
-    # video.show(frame, downsample_ratio=4)
-
+    converted_detections = convert_and_filter(detected_poses)
+    estimates = tracker.update(converted_detections, dt=1)
+    norfair.draw_points(frame, converted_detections)
+    norfair.draw_estimates(frame, estimates)
+    # video.write(frame)
+    video.show(frame, downsample_ratio=4)

norfair/drawing.py
@@ -1,0 +1,61 @@
+import cv2
+import numpy as np
+
+def draw_points(frame, detections, radius=None, thickness=None, color=None):
+    if radius is None:
+        frame_scale = frame.shape[0] * frame.shape[1] / 400000
+        radius = int(max(frame_scale * 1.3, 1))
+    if thickness is None:
+        frame_scale = frame.shape[0] * frame.shape[1] / 4000000
+        thickness = int(max(frame_scale * 1.4, 1))
+    if color is None:
+        color = Color.red
+
+    for d in detections:
+        for point in d:
+            cv2.circle(frame, tuple(point.astype(int)), radius=radius, color=color, thickness=thickness)
+
+def draw_estimates(frame, objects, radius=None, thickness=None, color=None, id_size=None):
+    frame_scale = frame.shape[0] * frame.shape[1] / 400000
+    if radius is None:
+        radius = int(frame_scale)
+    if id_size is None:
+        id_size = int(frame_scale / 5)
+    if thickness is None:
+        thickness = int(frame_scale / 5)
+    if color is None:
+        color = Color.blue
+
+    for obj in objects:
+        for point in obj.estimate:
+            cv2.circle(frame, tuple(point.astype(int)), radius=radius, color=color, thickness=-1)
+
+        id_draw_position = centroid(obj.estimate)
+        cv2.putText(
+            frame, str(obj.id), id_draw_position, cv2.FONT_HERSHEY_SIMPLEX, id_size,
+            Color.yellow, thickness, cv2.LINE_AA
+        )
+
+def centroid(points):
+    length = points.shape[0]
+    sum_x = np.sum(points[:, 0])
+    sum_y = np.sum(points[:, 1])
+    return int(sum_x/length), int(sum_y/length)
+
+class Color():
+    green = (0, 128, 0)
+    white = (255, 255, 255)
+    olive = (0, 128, 128)
+    black = (0, 0, 0)
+    navy = (128, 0, 0)
+    red = (0, 0, 255)
+    maroon = (0, 0, 128)
+    grey = (128, 128, 128)
+    purple = (128, 0, 128)
+    yellow = (0, 255, 255)
+    lime = (0, 255, 0)
+    fuchsia = (255, 0, 255)
+    aqua = (255, 255, 0)
+    blue = (255, 0, 0)
+    teal = (128, 128, 0)
+    silver = (192, 192, 192)